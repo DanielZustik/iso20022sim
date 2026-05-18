69f63993ba0bd5f70ac31d75c5513e59047a910e
Daniel Zuštík <dzustik@monetplus.cz>
2026-05-18 17:27:44 +0200

    v2run

---
+++ src/main/java/cz/monetplus/iso20022sim/validation/SchemaInvalidXmlException.java
@@ -0,0 +1,8 @@
+package cz.monetplus.iso20022sim.validation;
+
+public class SchemaInvalidXmlException extends RuntimeException {
+
+    public SchemaInvalidXmlException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
